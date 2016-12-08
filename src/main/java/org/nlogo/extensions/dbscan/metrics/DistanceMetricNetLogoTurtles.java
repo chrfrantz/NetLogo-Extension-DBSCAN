f83ba5a5cc1270255dafc08fb37c9378a7311f35
chrfrantz <cf@christopherfrantz.org>
2016-12-08 10:27:41 +0800

    Initial commit.

---
+++ src/main/java/org/nlogo/extensions/dbscan/metrics/DistanceMetricNetLogoTurtles.java
@@ -0,0 +1,37 @@
+package org.nlogo.extensions.dbscan.metrics;
+
+import org.christopherfrantz.dbscan.DBSCANClusteringException;
+import org.christopherfrantz.dbscan.DistanceMetric;
+import org.nlogo.agent.Turtle;
+import org.nlogo.api.AgentException;
+
+public class DistanceMetricNetLogoTurtles implements DistanceMetric<Turtle>{
+
+	private String field = null;
+	
+	public DistanceMetricNetLogoTurtles(String field) {
+		this.field = field;
+	}
+	
+	@Override
+	public double calculateDistance(Turtle val1, Turtle val2) throws DBSCANClusteringException {
+		try {
+			// Check on turtle level first ...
+			Double val1Num = Double.parseDouble(val1.getTurtleOrLinkVariable(field).toString());
+			Double val2Num = Double.parseDouble(val2.getTurtleOrLinkVariable(field).toString());
+			return Math.abs(val1Num - val2Num);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			try {
+				// ... before looking at breed level
+				Double val1Num = Double.parseDouble(val1.getBreedVariable(field).toString());
+				Double val2Num = Double.parseDouble(val2.getBreedVariable(field).toString());
+				return Math.abs(val1Num - val2Num);
+			} catch (NumberFormatException | AgentException e1) {
+	        	throw new DBSCANClusteringException(e1.getMessage());
+			}
+        } catch (NumberFormatException e) {
+        	throw new DBSCANClusteringException(e.getMessage());
+		}
+	}
+
+}
---
+++ src/test/java/org/nlogo/extensions/dbscan/TestNetLogoDBSCAN.java
@@ -0,0 +1,105 @@
+package org.nlogo.extensions.dbscan;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Random;
+
+import org.christopherfrantz.dbscan.DBSCANClusterer;
+import org.christopherfrantz.dbscan.DBSCANClusteringException;
+import org.junit.Test;
+import org.nlogo.agent.AgentSet;
+import org.nlogo.agent.Turtle;
+import org.nlogo.agent.World;
+import org.nlogo.api.AgentException;
+import org.nlogo.api.LogoList;
+import org.nlogo.api.LogoListBuilder;
+import org.nlogo.extensions.dbscan.metrics.DistanceMetricNetLogoTurtles;
+import org.nlogo.headless.HeadlessWorkspace;
+
+/**
+ * Tests DBSCAN clusterer using an existing model and injecting agents to be clustered.
+ * 
+ * @author Christopher Frantz <cf@christopherfrantz.org>
+ *
+ */
+
+public class TestNetLogoDBSCAN {
+	
+	@Test
+	public void testClusteringInExistingNetLogoModelFromJava() {
+		
+		final String TESTFILE = System.getProperty("user.dir") + 
+				"/src/test/resources/java-netlogo-clustering-test.nlogo";
+		
+		Random random = new Random(4522);
+		
+		final String AGENTS = "AGENTS";
+		final String VARIABLE = "WEALTH";
+		
+		HeadlessWorkspace ws = HeadlessWorkspace.newInstance();
+		
+		try {
+			ws.openString(org.nlogo.util.Utils.url2String("file://" + TESTFILE)); //System.getProperty("user.dir") + "/test/resources/java-netlogo-test.nlogo"));
+		} catch (Exception e1) {
+			fail("Problems when opening NetLogo model file: " + e1.getMessage());
+		}
+		
+		World world = ws.world;
+		
+		LogoListBuilder b = new LogoListBuilder();
+		AgentSet breed = world.getBreed(AGENTS);
+		
+		int i = 0;
+		while (i < 100) {
+			Turtle turtle = world.createTurtle(breed);
+			b.add(turtle);
+			i++;
+		}
+		
+		Collection<Turtle> l2 = new ArrayList<>();
+		
+		LogoList l1 = b.toLogoList();
+		
+		for (int j=0; j<l1.size(); j++) {
+			Turtle t = ((Turtle)l1.get(j));
+			
+			try {
+				t.setBreedVariable(VARIABLE, random.nextInt(1000));
+			} catch (AgentException e1) {
+				fail("Error setting variable value: " + e1);
+			}
+			
+			System.out.print("ID: " + t.id + ", Wealth: ");
+			
+			try {
+				System.out.println(t.getBreedVariable(VARIABLE));
+			} catch (AgentException e) {
+				fail("Error getting variable value: " + e);
+			}
+			
+			l2.add(t);
+		}
+		
+		int minCluster = 3;
+		double maxDistance = 5;
+		
+		ArrayList<ArrayList<Turtle>> result = null;
+		
+		try {
+			DBSCANClusterer<Turtle> clusterer = new DBSCANClusterer<Turtle>(l2, minCluster, maxDistance, new DistanceMetricNetLogoTurtles(VARIABLE));
+			result = clusterer.performClustering();
+		} catch (DBSCANClusteringException e) {
+			fail("Error during clustering operation: " + e);
+		}
+		
+		LogoList res = LogoList.fromJava(result);
+		
+		System.out.println("Resulting clusters: " + res);
+		
+		assertEquals("Number of Clusters equals", 12, res.size());
+	}
+
+}
